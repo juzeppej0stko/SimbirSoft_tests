46bf15f573a720d9d4edc8159980093535fbaf27
juzeppej0stko <cerber.c7@gmail.com>
2024-02-15 07:42:00 +0000

    Избавился от использования time.sleep. Изменил наименования некоторых методов для большего соответствия их логике. Учётная запись для авторизации была захардкожена в локаторе - Добавил параметр user_id в метод login для исправления этого замечания. Также добавил аннотации Allure к самому тесту для улучшения отчетности. Добавил проверку транзакций по сумме

--- pages/Banking_Page.py
+++ pages/Banking_Page.py
@@ -1,4 +1,3 @@
-import time
 import allure
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,14 +13,14 @@ class BankingPage:
         self.wait = WebDriverWait(self.driver, 10)
 
     @allure.step("Шаг 1: Открытие страницы авторизации")
-    def open_main_page(self):
+    def open_authorization_page(self):
         self.driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
 
     @allure.step("Шаг 2: Авторизация на сайте")
-    def login(self):
+    def login(self, user_id):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[ng-click='customer()']"))).click()
         self.wait.until(EC.presence_of_element_located((By.ID, "userSelect")))
-        Select(self.wait.until(EC.presence_of_element_located((By.ID, "userSelect")))).select_by_value("2")
+        Select(self.wait.until(EC.presence_of_element_located((By.ID, "userSelect")))).select_by_value(user_id)
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
         allure.attach(self.driver.get_screenshot_as_png(), name="LoginPage", attachment_type=allure.attachment_type.PNG)
 
@@ -35,10 +34,11 @@ class BankingPage:
         allure.attach(self.driver.get_screenshot_as_png(), name="DepositPage",
                       attachment_type=allure.attachment_type.PNG)
 
-    @allure.step("Шаг 4: Списание со счета")
+    @allure.step("Шаг 4: Снятие со счета")
     def withdraw(self, amount):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[ng-click='withdrawl()']"))).click()
-        time.sleep(2)
+        # Ожидание появления элемента "Amount to be Withdrawn"
+        self.wait.until(EC.presence_of_element_located((By.XPATH, "//label[text()='Amount to be Withdrawn :']")))
         self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[placeholder='amount']"))).send_keys(
             str(amount))
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
@@ -46,14 +46,12 @@ class BankingPage:
                       attachment_type=allure.attachment_type.PNG)
 
     @allure.step("Шаг 5: Проверка, что баланс равен 0")
-    def check_balance(self):
+    def check_balance_is_zero(self):
         return self.wait.until(
             EC.text_to_be_present_in_element((By.XPATH, "//strong[@class='ng-binding'][text()='0']"), "0"))
 
-    time.sleep(2)
-
-    @allure.step("Шаг 6: Проверка транзакций")
-    def check_transactions(self):
+    @allure.step("Шаг 6: Проверка страницы транзакций")
+    def check_transactions_page(self):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[ng-click='transactions()']"))).click()
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//td[text()='Credit']")))
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//td[text()='Debit']")))
@@ -69,7 +67,14 @@ class BankingPage:
             data.append([col.text for col in cols])
         return data
 
-    @allure.step("Шаг 8: Запись данных в CSV и прикрепление файла к отчету Allure")
+    @allure.step("Шаг 8: Проверка транзакций по сумме")
+    def check_transactions_by_amount(self, deposit_amount, withdraw_amount):
+        data = self.get_transaction_data()
+        deposit_transaction = [row for row in data if row[1] == str(deposit_amount) and row[2] == 'Credit']
+        withdraw_transaction = [row for row in data if row[1] == str(withdraw_amount) and row[2] == 'Debit']
+        return deposit_transaction and withdraw_transaction
+
+    @allure.step("Шаг 9: Запись данных в CSV и прикрепление файла к отчету Allure")
     def get_and_write_transaction_data(self):
         data = self.get_transaction_data()
 
--- tests/test_banking_page.py
+++ tests/test_banking_page.py
@@ -1,13 +1,36 @@
+import allure
 from pages.Banking_Page import BankingPage
 from utils import fib_number
 
 
+@allure.feature('Banking Page Transactions')
+@allure.story('Transaction Test')
+@allure.severity(allure.severity_level.CRITICAL)
 def test_transactions(driver):
     banking_page = BankingPage(driver)
-    banking_page.open_main_page()
-    banking_page.login()
-    banking_page.deposit(fib_number)
-    banking_page.withdraw(fib_number)
-    banking_page.check_balance()
-    banking_page.check_transactions()
-    banking_page.get_and_write_transaction_data()
+    deposit_amount = fib_number
+    withdraw_amount = fib_number
+
+    with allure.step("Open authorization page"):
+        banking_page.open_authorization_page()
+
+    with allure.step("Login with user 2"):
+        banking_page.login("2")  # "2" - Harry Potter
+
+    with allure.step("Deposit"):
+        banking_page.deposit(deposit_amount)
+
+    with allure.step("Withdraw"):
+        banking_page.withdraw(withdraw_amount)
+
+    with allure.step("Check balance is zero"):
+        assert banking_page.check_balance_is_zero()
+
+    with allure.step("Check transactions page"):
+        banking_page.check_transactions_page()
+
+    with allure.step("check transactions by amount"):
+        assert banking_page.check_transactions_by_amount(deposit_amount, withdraw_amount)
+
+    with allure.step("Get and write transaction data"):
+        banking_page.get_and_write_transaction_data()
